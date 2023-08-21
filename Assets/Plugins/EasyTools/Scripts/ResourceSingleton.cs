18cc4d8ae2ada72bafa9b76a26ee560f29fd066a
GuanmingLu <40348297+GuanmingLu@users.noreply.github.com>
2023-08-21 21:54:43 +0800

    整理Settings相关

--- Assets/Plugins/EasyTools/Scripts/ResourceSingleton.cs
+++ Assets/Plugins/EasyTools/Scripts/ResourceSingleton.cs
@@ -8,20 +8,20 @@ using UnityEditor;
 #endif
 
 namespace EasyTools.Settings {
-	public class ResourceSingleton<T> : ScriptableObject where T : ScriptableObject {
-		private static string _fileName => $"DontRename_{typeof(T).Name}_Singleton";
+	public class ResourceSingleton<T> : ScriptableObject where T : ResourceSingleton<T> {
+		private static string FileName => $"{typeof(T).Namespace}.{typeof(T).Name}";
 
 		private static T _instance;
 		protected static T Instance {
 			get {
 				if (_instance != null) return _instance;
-				_instance = Resources.Load<T>(_fileName);
+				_instance = Resources.Load<T>(FileName);
 #if UNITY_EDITOR
 				if (_instance == null) {
 					_instance = CreateInstance<T>();
 					var dir = Path.GetDirectoryName(AssetDatabase.GetAssetPath(MonoScript.FromScriptableObject(_instance)));
 					if (!AssetDatabase.IsValidFolder($"{dir}/Resources")) AssetDatabase.CreateFolder(dir, "Resources");
-					AssetDatabase.CreateAsset(_instance, $"{dir}/Resources/{_fileName}.asset");
+					AssetDatabase.CreateAsset(_instance, $"{dir}/Resources/{FileName}.asset");
 					AssetDatabase.SaveAssets();
 					AssetDatabase.Refresh();
 					EditorUtility.SetDirty(_instance);
